beb855a19e0cc880cc6b3f481f6b58c77b427afa
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-09-02 17:14:40 +0300

    Inheritance Mapping

--- EF-Core-4/Program.cs
+++ EF-Core-4/Program.cs
@@ -1,6 +1,7 @@
 ﻿using EF_Core_4.Data;
 using EF_Core_4.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace EF_Core_4
 {
@@ -32,12 +33,26 @@ namespace EF_Core_4
 
             #region DML [Data Manipulation Language]
 
-            int rowsAffected = context.Database.ExecuteSqlRaw(
-                                                         "UPDATE Products SET UnitPrice = UnitPrice * 1.1 WHERE CategoryID = {0}", 2);
+            //int rowsAffected = context.Database.ExecuteSqlRaw(
+            //                                             "UPDATE Products SET UnitPrice = UnitPrice * 1.1 WHERE CategoryID = {0}", 2);
 
-            Console.WriteLine($"{rowsAffected} rows were updated.");
+            //Console.WriteLine($"{rowsAffected} rows were updated.");
             #endregion
 
+
+
+
+            #region Inheritance Mapping 
+
+            //1-- TPH [Table Per Hierarchy]
+            //    Parent table stores all entities in the hierarchy and uses a discriminator column to differentiate between them.
+            //2-- TPT [Table Per Type]
+            //    Each entity in the hierarchy is mapped to its own table, with relationships established between them.
+            //3-- TPC [Table Per Concrete Class]
+            //    Each concrete class in the hierarchy is mapped to its own table, containing all properties of the class.
+
+
+            #endregion
         }
     }
 }
